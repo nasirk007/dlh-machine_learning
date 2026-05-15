88dab0727145c1669095987828a581deddf09925
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-15 19:16:42 +0000

    transpose matrix 2D

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -3,5 +3,18 @@
 
 
 def matrix_transpose(matrix):
-    """Returns the transpose of a 2D matrix"""
-    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    """Returns the transpose of a 2D matrix."""
+    transpose = []
+
+    # iterate over columns
+    for j in range(len(matrix[0])):
+        new_row = []
+
+        # iterate over rows
+        for i in range(len(matrix)):
+            new_row.append(matrix[i][j])
+
+        # add the constructed row to result
+        transpose.append(new_row)
+
+    return transpose
