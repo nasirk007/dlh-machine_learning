f037f45a531e29b4cf9228830d1becbf764bee86
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 18:55:31 +0000

    mathematcial operation on matrix become simple after numpy library

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this task is about element-wise mathametical operations matric."""
+
+
+def np_elementwise(mat1, mat2):
+    """this is documentation for mathematical operation on matrix."""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
