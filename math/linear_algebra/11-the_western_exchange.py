81860a0a1f5d4cbbc75e21242ee93ef365983fa3
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 15:11:14 +0000

    matrix transposition using built-in numpy method

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this task isto get tranpose of matrix using built-in numpy library."""
+
+
+def np_transpose(matrix):
+    """this is documentation for transposing matrix."""
+    new_matrix = np.transpose(matrix)
+    return new_matrix
