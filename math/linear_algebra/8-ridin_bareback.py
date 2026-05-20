d5f6e073c81a3c2e6c4658107ea6e9fcad50a167
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 13:52:07 +0000

    m2D atrix multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def mat_mul(mat1, mat2):
+    if len(mat1[0]) != len(mat2):
+        return None
+    result_mat = []
+    for i in range(len(mat1)):
+        row = []
+        for k in range(len(mat2[0])):
+            P = 0  # initialise sum after multiplication
+            for j in range(len(mat2)):
+                x = mat1[i][j] * mat2[j][k]
+                P += x
+            row.append(P)
+        result_mat.append(row)
+    return result_mat
