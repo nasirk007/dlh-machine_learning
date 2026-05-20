f2af439408c25a2ec8b399036929d6d1900ddd24
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-20 19:08:19 +0000

    cancatenation of matrix axis by axis

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """this task isto perform cancatenation on matrix."""
 
-
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """this is documentation to doing cancatenation of matrix."""
-    new_matrix = np.concatenate((mat1, mat2, axis=axis))
+    new_matrix = np.concatenate((mat1, mat2), axis=axis)
     return new_matrix
