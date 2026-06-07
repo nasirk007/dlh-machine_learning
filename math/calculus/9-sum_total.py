826c55f55991f079bfebe82a1e61335305996042
Nasir9372 <nasirkhan9372@gmail.com>
2026-06-07 01:23:47 +0000

    without loop, sum all squared values from 1 to n including n

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """Returns the summation of i^2"""
-    if not isinstance(n, (int, float)):
+    if not isinstance(n, int) or n <= 0:
         return None
     else:
-        return int(n * (n + 1) * (2 * n + 1) / 6)
+        return int(n*(n + 1)*(2*n + 1)/6)
