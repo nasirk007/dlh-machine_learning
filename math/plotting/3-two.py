009bb45f9b9ba35744f9491e040c6e49866e3a5f
nsrcap <nsr.capital.007@gmail.com>
2026-06-11 13:53:38 +0200

    line graph where y values = e^(decay rate * time on x-axis), like y value will be btw 0 to 1 and it will start from 1 and keep falling (log) over the year continuous with rapid speed (exponential)

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
-"""Module that plots y as a scatter graph"""
+"""Module that plots two line graph using one input x"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def change_scale():
-    """use value of x to plot y as a scatter graph"""
-    x = np.arange(0, 28651, 5730)
+def two():
+    """this is documentation to plots two line graph using one input x"""
+    x = np.arange(0, 21000, 1000)
+    # This represents a decay rate at 50%
+    # log convert it into rate of -0.693
     r = np.log(0.5)
-    t = 5730
-    y = np.exp((r / t) * x)
+    # over time decay, and these are decay speed
+    t1 = 5730
+    t2 = 1600
+    # continuous decay formula, give the fraction remaining at time x
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    # set size of the graph
     plt.figure(figsize=(6.4, 4.8))
 
     # my code is here
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of C-14")
-    plt.xlim(0, 28650)
-    #  It will only changes how we display the axis, log is for declining axis
-    plt.yscale("log")
-    plt.plot(x, y)
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x, y1, 'r-', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
+    plt.legend(loc='upper right')
     plt.show()
+    plt.savefig('exponential_decay.png')
+
+
+two()
