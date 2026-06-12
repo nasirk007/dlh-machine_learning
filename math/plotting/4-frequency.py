00c847483c36921e4c6f7955d7c1f9fbec1e9b6c
nsrcap <nsr.capital.007@gmail.com>
2026-06-12 06:17:21 +0200

    creating histogram with controlled axis

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module that plots two line graph using one input x"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """this is documentation to plots two line graph using one input x"""
+    # generate same sequence of random number every time we run the code
+    np.random.seed(5)
+    # generate 50 random data points with mean 68 and standard deviation 15
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    # create x and y axis values auto using hist, with 50 random data points
+    # each data point represent student grades
+    # hist will auto calculate range of grades (min and max) from 50 random no.
+    # lets say mini grade was 50 and maxi was 96 = 46/10bin = 4.6 approx 5
+    # from start 50+5=55 so first range is 50-55 and so on
+    # this represent value on x-axis
+    # Y-axis value, hist auto counts students falling in each range
+    plt.hist(student_grades, bins=interval, edgecolor="black")
+    # understood from visual provided in task, rather explicit statement
+    interval = np.arange(0, 101, 10)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    # understood from visual provided in task, rather explicit statement
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.show()
