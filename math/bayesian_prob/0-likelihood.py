09e350884def902c3401a223d3453fe7bfb3c10d
nsrcap <nsr.capital.007@gmail.com>
2026-06-19 07:42:11 +0200

    likelihood function = pmf of binomial distribtion, and it is P(B|A) or P(data of patients observed when probability of side effect was initially given like 0.5, but this p=0.5 get change to multiple value as a list, becuase p was fixed parameter in BD before and now continous random variable, so each result of each patient trail will not have prob of side effect as 0.5

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -7,7 +7,7 @@ def likelihood(x, n, P):
     "find likelihood of patients taking drugs result in severe side effects."
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int):
+    if not isinstance(x, int) or not x >= 0:
         raise ValueError("x must be an integer and greater than or == 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
