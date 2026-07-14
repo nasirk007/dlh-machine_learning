3da79696b9a165c6ca49ad369abb67f103f7f8d5
NK <nsr.capital.007@outlook.com>
2026-07-14 19:20:24 +0200

    concat two table

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -9,6 +9,6 @@ def concat(df1, df2):
     df1 = index(df1)
     df2 = index(df2)
     df2 = df2.iloc[:1417411920]
-    df3 = [df1, df2]
+    df3 = [df2, df1]
     df = pd.concat(df3, keys=['bitstamp', 'coinbase'])
     return df
