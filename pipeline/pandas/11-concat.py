4c23c7fca3b9611b6ad7b83e2928dd9e4ea71c93
NK <nsr.capital.007@outlook.com>
2026-07-14 19:17:33 +0200

    concat two table

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """Module that concate two objects referred as DataFrame."""
+import pandas as pd
+index = __import__('10-index').index
 
 
 def concat(df1, df2):
-    """Function that concat two DataFrames."""
-    df1 =
-    df2 =
+    """Task that concate two DataFrames."""
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2.iloc[:1417411920]
+    df = pd.concat(df2, df1, keys=['bitstamp', 'coinbase'])
     return df
